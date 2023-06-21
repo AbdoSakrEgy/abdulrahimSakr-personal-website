65099257477a61df907d8810eaff055e440fc54b
Abdel Raheem <AbdelRaheemSakr@gmail.com>
2023-06-21 22:11:23 +0300

    deploy don!-but remove react-router library code because it cause erro

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <SiteRoot/>
   </React.StrictMode>,
 )
