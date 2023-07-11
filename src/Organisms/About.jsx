1fbb056ca3c71b61964849923258040258278e51
Abdel Raheem <AbdelRaheemSakr@gmail.com>
2023-07-11 13:42:08 +0300

    update profile image

--- src/Organisms/About.jsx
+++ src/Organisms/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import personalimg from "../assets/Personal image.jpg";
+import personalimg from "../assets/Personal image.jpeg";
 import { Icon } from "@iconify/react";
 
 export default function About() {
